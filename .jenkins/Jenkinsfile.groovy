57b48fa6a9a36fa3438fd3685330794e1cdf6cfd
Daniel Schulz (Madeline) <faulolio@gmail.com>
2025-11-07 21:36:24 +0100

    devops: fix setup

--- .jenkins/Jenkinsfile.groovy
+++ .jenkins/Jenkinsfile.groovy
@@ -6,23 +6,20 @@ pipeline {
 		disableConcurrentBuilds()
 		disableResume()
 	}
+	environment {
+		UNITY_CREDENTIALS = credentials('Slothsoft-Unity')
+		EMAIL_CREDENTIALS = credentials('Slothsoft-Google')
+	}
 	stages {
 		stage('Index workspace') {
 			steps {
 				script {
-					stash name: 'data', allowEmpty: false, includes: 'Data/**'
-
 					unityProject (
 							// define unity project location relative to repository
 							LOCATION : '',
 
-							// If given, automatically use these credentials to license a free Unity version.
-							UNITY_CREDENTIALS : 'Slothsoft-Unity',
-							EMAIL_CREDENTIALS : 'Slothsoft-Google',
-							STEAM_CREDENTIALS : 'Slothsoft-Steam',
-
 							// use auto-versioning based on tags+commits
-							AUTOVERSION : '',
+							AUTOVERSION : 'git',
 
 							// automatically create C# docs
 							BUILD_DOCUMENTATION : '0',
@@ -35,35 +32,27 @@ pipeline {
 							FORMATTING_EXCLUDE : 'Library Assets/Plugins Packages',
 
 							// which executables to create
-							BUILD_FOR_WINDOWS : '1',
+							BUILD_FOR_WINDOWS : '0',
 							BUILD_FOR_LINUX : '0',
 							BUILD_FOR_MAC : '0',
 							BUILD_FOR_WEBGL : '1',
 							BUILD_FOR_ANDROID : '0',
 
-							BUILD_WINDOWS_CALL : { project, build, report ->
-								callUnity "unity-build '${project}' '${build}' windows", report
-								dir(build) {
-									unstash 'data'
-								}
-							},
-
 							// which platforms to deploy to
 							DEPLOY_TO_STEAM : '0',
 							DEPLOY_TO_ITCH : '0',
 							DEPLOY_ON_FAILURE : '1',
-							DEPLOYMENT_BRANCHES : ["/main"],
 
 							// configration for deploying to steam
 							STEAM_ID : '',
 							STEAM_DEPOT_WINDOWS : '',
 							STEAM_DEPOT_LINUX : '',
 							STEAM_DEPOT_MAC : '',
-							STEAM_BRANCH : '',
+							STEAM_CREDENTIALS : '',
 
 							// configuration for deploying to itch
-							ITCH_ID : '',
-							ITCH_CREDENTIALS : '',
+							ITCH_ID : 'faulo/blooming-community',
+							ITCH_CREDENTIALS : 'Faulo-itch.io',
 
 							// configuration for deploying to Discord
 							REPORT_TO_DISCORD : '0',
